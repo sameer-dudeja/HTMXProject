14c89e61f8b991143ae35b811714e6bbd86fded4
Sameer Dudeja <dsameer33@gmail.com>
2024-06-20 04:10:13 +0530

    Initial Commit for the HTMX app

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log("Server is running on Port 3000")
+})
